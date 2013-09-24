906d3f7128e4cb300c83c25dfd722c85f78ee18b
Tim Düsterhus <timwolla@bastelstu.be>
2013-09-24 16:42:49 +0200

    Clean up blacklisted code

---
+++ file/lib/system/event/listener/DailyCleanUpCronjobExecuteTwofaCleanUpListener.class.php
@@ -0,0 +1,26 @@
+<?php
+namespace chat\system\event\listener;
+
+/**
+ * Vaporizes unneeded data.
+ *
+ * @author 	Tim Düsterhus
+ * @copyright	2012-2013 Tim Düsterhus
+ * @license	BSD 3-Clause License <http://opensource.org/licenses/BSD-3-Clause>
+ * @package	be.bastelstu.wcf.twofa
+ * @subpackage	system.event.listener
+ */
+class DailyCleanUpCronjobExecuteTwofaCleanUpListener implements \wcf\system\event\IEventListener {
+	/**
+	 * @see	\wcf\system\event\IEventListener::execute()
+	 */
+	public function execute($eventObj, $className, $eventName) {
+		// clean up blacklisted codes
+		$sql = "DELETE FROM	wcf".WCF_N."_user_twofa_blacklist
+			WHERE		time < ?";
+		$statement = \wcf\system\WCF::getDB()->prepareStatement($sql);
+		$statement->execute(array(
+			(TIME_NOW - 86400 * 30)
+		));
+	}
+}
